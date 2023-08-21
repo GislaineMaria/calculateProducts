1285108135e6c56b39243e0d40135d818a3fea0a
Gislaine Gavlak <gislainegavlak@gmail.com>
2023-08-21 19:38:26 -0300

    feat: flix

---
+++ src/test/java/com/devsuperior/calculateproducts/CalculateproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.calculateproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
